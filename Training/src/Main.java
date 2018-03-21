ca59cd0608c8ee66aed262dad61dd562071b7b70
Robert <soarerobert17@gmail.com>
2018-03-21 08:47:40 +0200

    Server

--- Training/src/Main.java
+++ Training/src/Main.java
@@ -2,7 +2,16 @@
 
 public class Main {
 
-    public static void main(String[] args) {
-	    System.out.println("Hello world!");
+    public static void main(String[] args)
+    {
+	    SimpleServer server = new SimpleServer();
+	    SimpleClient client = new SimpleClient("localhost", 40404);
+
+	    server.Start();
+        client.Connect();
+        server.Run();
+
+        client.Send("Oala!");
+        server.Received();
     }
 }
